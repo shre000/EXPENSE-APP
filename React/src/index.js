81974471b173d0509b1ad026dd1b6eea263c6d0e
shreya suman <sumanshreya252@gmail.com>
2022-06-13 11:57:31 +0530

    minor

--- React/src/index.js
+++ React/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import  ReactDom  from "react-dom";
 import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './app';
@@ -9,5 +8,4 @@ const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />);
 
-// ReactDom.render(<App />,  document.getElementById('root')); //App component is passing
 
